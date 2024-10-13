050c56a56cd8dd662c443aa936db3ff167441026
Iv9eni <ivgeni.darinski@hotmail.com>
2024-10-13 03:03:58 -0400

    autocomplete items

--- src/lib/components/TransactionCategory.svelte
+++ src/lib/components/TransactionCategory.svelte
@@ -1,7 +1,59 @@
 <script lang="ts">
+<<<<<<< Updated upstream
     export let value;
 </script>
 
 <td>
     {value}
+=======
+    import { Autocomplete, popup } from "@skeletonlabs/skeleton";
+    import type { AutocompleteOption, PopupSettings } from '@skeletonlabs/skeleton';
+
+    export let value : string;
+    let search : string = value;
+    let isEditting : boolean = false;
+
+    const categories: AutocompleteOption<string>[] = [
+        {
+            label: 'Car', value: 'car' 
+        },
+        {
+            label: 'Entertainment', value: 'entertainment'
+        },
+        {
+            label: 'Groceries', value: 'groceries'
+        }
+    ];
+
+    let popupSettings: PopupSettings = {
+        event: 'focus-click',
+        target: 'popupAutocomplete',
+        placement: 'bottom',
+    };
+    
+    const onCategorySelection = (e: CustomEvent<AutocompleteOption<string>>): void => {
+        search = e.detail.label;
+    }
+
+</script>
+
+<td on:click={() => isEditting = true} >
+    {#if isEditting}
+        <input
+            class="input autocomplete"
+            type="search"
+            name="autocomplete-search"
+            bind:value={search}
+            placeholder="Search..."
+            use:popup={popupSettings} />
+        <div data-popup="popupAutocomplete" tabindex="-1">
+            <Autocomplete
+                bind:input={search}
+                options={categories}
+                on:selection={onCategorySelection} />
+        </div>
+    {:else}
+        {search}
+    {/if}
+>>>>>>> Stashed changes
 </td>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import type { TransactionRowType } from '$lib/app/types';
     import Upload from '$lib/components/Upload.svelte';
     import TransactionTable from '$lib/components/TransactionTable.svelte';
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-
+    
     let file : File;
     let transactions : TransactionRowType[] = [];
 
@@ -29,7 +29,7 @@
         <LightSwitch />
     </div>
 
-    { #if file != null }
+    { #if file }
     <div class="flex-grow flex flex-col items-center justify-center w-screen mb-4">
         <div class="w-3/4 mb-6">
             <h1 class="text-4xl font-bold">Transactions</h1>
