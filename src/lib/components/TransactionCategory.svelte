3e7e997621b6323a554331e41141c1ea5356504b
Iv9eni <ivgeni.darinski@hotmail.com>
2024-10-13 02:52:21 -0400

    add header margin

--- src/lib/components/TransactionCategory.svelte
+++ src/lib/components/TransactionCategory.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-	import { Autocomplete } from "@skeletonlabs/skeleton";
-    import type { AutocompleteOption } from '@skeletonlabs/skeleton';
-
-    let isEditting : boolean = false;
-    let search : string = '';
-
-    export let value : string;
-    export const categories : string[] = ['Car', 'Entertainment', 'Groceries'];
+    export let value;
 </script>
 
-<td on:click={() => isEditting = !isEditting}>
-    {#if isEditting}
-        <Autocomplete allowlist={categories} />
-    {:else}
-        {value}
-    {/if}
+<td>
+    {value}
 </td>
